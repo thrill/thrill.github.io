a24e477a04231fb4fb46aa7735b2fe13c1d85f8f
mk-doxygen (Travis CI) <thrill@panthema.net>
2017-05-10 11:55:19 +0000

    Deploy code docs to GitHub Pages Travis build: 1103
    Commit: d49375aa3b1ba0be5ed00473b0613559101dc38c

--- docs/master/classexamples_1_1stochastic__gradient__descent_1_1StochasticGradientDescent.js
+++ docs/master/classexamples_1_1stochastic__gradient__descent_1_1StochasticGradientDescent.js
@@ -1,7 +1,7 @@
 var classexamples_1_1stochastic__gradient__descent_1_1StochasticGradientDescent =
 [
     [ "StochasticGradientDescent", "classexamples_1_1stochastic__gradient__descent_1_1StochasticGradientDescent.html#a639d5b86a5c8bdfd9af1b6f99c40d02c", null ],
-    [ "is_converged", "classexamples_1_1stochastic__gradient__descent_1_1StochasticGradientDescent.html#a9bd191da23065d98ea86c3949c781871", null ],
+    [ "is_converged", "classexamples_1_1stochastic__gradient__descent_1_1StochasticGradientDescent.html#a011d1532593e27cca85ffb9987ae32d2", null ],
     [ "optimize", "classexamples_1_1stochastic__gradient__descent_1_1StochasticGradientDescent.html#aa4dfe84370d449c6091a0f5164d776cd", null ],
     [ "mini_batch_fraction", "classexamples_1_1stochastic__gradient__descent_1_1StochasticGradientDescent.html#ad52209dab1d2bccb346f98acdf814703", null ],
     [ "num_iterations", "classexamples_1_1stochastic__gradient__descent_1_1StochasticGradientDescent.html#a22fec0af810a755e77ecbb7179c193d4", null ],
--- docs/master/structexamples_1_1stochastic__gradient__descent_1_1GradientResult.js
+++ docs/master/structexamples_1_1stochastic__gradient__descent_1_1GradientResult.js
@@ -1,6 +1,6 @@
 var structexamples_1_1stochastic__gradient__descent_1_1GradientResult =
 [
-    [ "operator+", "structexamples_1_1stochastic__gradient__descent_1_1GradientResult.html#a58202a83ad063f3b5915924c6ad4a764", null ],
+    [ "operator+", "structexamples_1_1stochastic__gradient__descent_1_1GradientResult.html#aaf9f0c54bba19f4881df779c9ca6078f", null ],
     [ "serialize", "structexamples_1_1stochastic__gradient__descent_1_1GradientResult.html#af1bc3875f801cb81d66f3076f3094267", null ],
     [ "loss", "structexamples_1_1stochastic__gradient__descent_1_1GradientResult.html#a968cec49c06f32d24c5035cc51a3afad", null ],
     [ "weights", "structexamples_1_1stochastic__gradient__descent_1_1GradientResult.html#a2da256b74c049d8736ff4bf8d0343495", null ]
